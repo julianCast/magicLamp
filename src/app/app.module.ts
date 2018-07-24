6f3916dca6db7f792c179aad3217f333f87f9348
JulianCast <squeleton@gmail.com>
2018-07-24 18:53:14 +0200

    First version of breathLamp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Insomnia } from '@ionic-native/insomnia';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Insomnia,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Insomnia } from '@ionic-native/insomnia';
+
+declare var $: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,87 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private lightIsOn : boolean = false
+  protected turningOff : boolean = false
+  protected showQuickInfo : boolean = false
+  private turnOffTimer: any = null
+  private minutesToTurnOff: number = 8
+  private lightMode: string = 'inOut'
 
+  //pulse effect
+  private secondsToInhale: number = 4
+  private secondsToIExhale: number = 6
+
+  constructor(
+    public navCtrl: NavController,
+    public insomnia: Insomnia
+  ) {}
+
+  protected startPulseBucle(): void
+  {
+    $('#light').animate({
+      backgroundColor: '#ce7b00'
+    }, this.secondsToInhale*1000, () => {
+      if (this.lightIsOn) {
+        $('#light').animate({
+          backgroundColor: '#ce4c00'
+        }, 500, () => {
+          $('#light').animate({
+            backgroundColor: 'black'
+          }, this.secondsToIExhale*1000, () => {
+            if (this.lightIsOn) {
+              this.startPulseBucle()
+            } else {
+              $('#light').css({
+                backgroundColor: 'black',
+                opacity: 1
+              })
+              this.turningOff = false;
+            }
+          });
+        })
+      } else{
+        $('#light').css({
+          backgroundColor: 'black',
+          opacity: 1
+        })
+        this.turningOff = false;
+      } 
+    });
+  }
+
+  protected turnOn(): void
+  {
+    this.lightIsOn =  true
+    this.startPulseBucle()
+    this.insomnia.keepAwake()
+    this.turnOffTimer = setTimeout(() => {
+      this.turnOff()
+    }, this.minutesToTurnOff*60*1000)
+  }
+
+  protected changeTime(): void
+  {
+    this.minutesToTurnOff = this.minutesToTurnOff == 8 ? 20 : 8
+  }
+
+  protected changeMode(): void
+  {
+    this.lightMode = this.lightMode == 'inOut' ? '478' : 'inOut'
+    this.showQuickInfo = true
+    console.log(this.showQuickInfo)
+    setTimeout(() => {
+      this.showQuickInfo = false
+    }, 2000);
+  }
+
+  protected turnOff(): void
+  {
+    this.lightIsOn =  false 
+    this.turningOff = true
+    this.insomnia.allowSleepAgain()
+    clearTimeout(this.turnOffTimer)
+    $('#light').css({opacity: 0})
   }
 
 }
