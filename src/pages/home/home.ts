6c5320d22c1f7cf0b67c4e1555487df49f4db759
JulianCast <squeleton@gmail.com>
2018-07-25 21:21:23 +0200

    Added 478 method and stop jquery function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,50 +11,77 @@ declare var $: any;
 export class HomePage {
 
   private lightIsOn : boolean = false
-  protected turningOff : boolean = false
-  protected showQuickInfo : boolean = false
+  protected hideQuickInfo : boolean = false
   private turnOffTimer: any = null
   private minutesToTurnOff: number = 8
   private lightMode: string = 'inOut'
 
-  //pulse effect
-  private secondsToInhale: number = 4
-  private secondsToIExhale: number = 6
+  // inOut Mode
+  private secondsToInhale_inOut: number = 4
+  private colorToInhale_inOut: string = "#ce7b00"
+  
+  private secondsToExhale_inOut: number = 6
+  private colorToExhale_inOut: string = "#ce4c00"
 
+  // 478 Mode
+  private secondsToInhale_478: number = 4
+  private colorToInhale_478: string = "#ce7b00"
+  
+  private secondsToHoldBreathe_478: number = 7
+  private colorToHoldBreathe_478: string = "#ff3b34"
+  
+  private secondsToExhale_478: number = 8
+  private colorToExhale_478: string = "#ce4c00"
+  private timeOutHoldBreath_478: any
   constructor(
     public navCtrl: NavController,
     public insomnia: Insomnia
   ) {}
 
-  protected startPulseBucle(): void
+  protected startInOutMode(): void
   {
     $('#light').animate({
-      backgroundColor: '#ce7b00'
-    }, this.secondsToInhale*1000, () => {
+      backgroundColor: this.colorToInhale_inOut
+    }, this.secondsToInhale_inOut*1000, () => {
       if (this.lightIsOn) {
         $('#light').animate({
-          backgroundColor: '#ce4c00'
+          backgroundColor: this.colorToExhale_inOut
         }, 500, () => {
           $('#light').animate({
             backgroundColor: 'black'
-          }, this.secondsToIExhale*1000, () => {
+          }, this.secondsToExhale_inOut*1000, () => {
             if (this.lightIsOn) {
-              this.startPulseBucle()
-            } else {
-              $('#light').css({
-                backgroundColor: 'black',
-                opacity: 1
-              })
-              this.turningOff = false;
+              this.startInOutMode()
             }
           });
         })
-      } else{
-        $('#light').css({
-          backgroundColor: 'black',
-          opacity: 1
+      }
+    });
+  }
+
+  protected start478Mode(): void
+  {
+    $('#light').animate({
+      backgroundColor: this.colorToInhale_478
+    }, this.secondsToInhale_478*1000, () => {
+      if (this.lightIsOn) {
+        $('#light').animate({
+          backgroundColor: this.colorToHoldBreathe_478
+        }, 500, () => {
+          this.timeOutHoldBreath_478 = setTimeout(() => {
+            $('#light').animate({
+              backgroundColor: this.colorToExhale_478
+            },500, () => {
+              $('#light').animate({
+                backgroundColor: 'black'
+              }, this.secondsToExhale_478 * 1000, () => {
+                if (this.lightIsOn) {
+                  this.start478Mode()
+                }
+              });
+            })
+          }, this.secondsToHoldBreathe_478*1000);
         })
-        this.turningOff = false;
       } 
     });
   }
@@ -62,8 +89,19 @@ export class HomePage {
   protected turnOn(): void
   {
     this.lightIsOn =  true
-    this.startPulseBucle()
     this.insomnia.keepAwake()
+    setTimeout(() => {
+      this.hideQuickInfo = true
+    }, 2000);
+    switch (this.lightMode) {
+      case "inOut":
+        this.startInOutMode()
+        break;
+        case "478":
+        this.start478Mode()
+        break;
+    }
+   
     this.turnOffTimer = setTimeout(() => {
       this.turnOff()
     }, this.minutesToTurnOff*60*1000)
@@ -77,20 +115,18 @@ export class HomePage {
   protected changeMode(): void
   {
     this.lightMode = this.lightMode == 'inOut' ? '478' : 'inOut'
-    this.showQuickInfo = true
-    console.log(this.showQuickInfo)
-    setTimeout(() => {
-      this.showQuickInfo = false
-    }, 2000);
   }
 
   protected turnOff(): void
   {
     this.lightIsOn =  false 
-    this.turningOff = true
     this.insomnia.allowSleepAgain()
+    this.hideQuickInfo = false
+
     clearTimeout(this.turnOffTimer)
-    $('#light').css({opacity: 0})
+    clearTimeout(this.timeOutHoldBreath_478)
+    $('#light').stop()
+    $('#light').css({backgroundColor: "black"})
   }
 
 }
